b7c12380f31950ce6d3398fdee22d29d2626580d
jlin03 <43045402+jlin03@users.noreply.github.com>
2019-11-26 18:55:37 -0500

    Update parse_args.c

--- parse_args.c
+++ parse_args.c
@@ -1,11 +1,29 @@
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 char ** parse_args(char * line) {
+    char * copy = strdup(line);
     char ** args;
-    args[0] = strsep(line," ");
-    int i = 1;
-    while(args[i] = strdup(*(args + strlen(args[i-1]) + 1))) {
-      strsep(args[i]," ");
+    int i = 0;
+    char * iter;
+    args = malloc(5*sizeof(char*));
+    while((iter = strsep(&copy," ")) != NULL) {
+        args[i] = malloc(sizeof(char*));
+        strcpy(args[i],iter);
+        i++;
     }
     return args;
+}
+
+
+int main() {
+    char * cmd = "ls -a -l";
+    char * x = malloc(sizeof(char*));
+    strcpy(x,cmd);
+    char ** args = parse_args(x);
+    printf("%s",args[0]);
+    execvp(args[0], args);
+    return 0;
 }
